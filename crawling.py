66ed47ba95fe678be3ee28e090779ad9b075c537
basekim14 <basekim14@gmail.com>
2020-07-04 22:03:46 +0900

    Update crawling function (select > find)

--- crawling.py
+++ crawling.py
@@ -1,5 +1,4 @@
 #-*- coding:utf-8 -*-
-
 from bs4 import BeautifulSoup
 import requests
 
@@ -7,11 +6,30 @@ url = "http://builder.hufs.ac.kr/user/indexSub.action?codyMenuSeq=37080&siteId=h
 res = requests.get(url)
 soup = BeautifulSoup(res.content, "html.parser")
 
-tr_tags = soup.select("#board-container > div.list > form:nth-child(2) > table > tbody > tr")
+# tr_tags = soup.select("#board-container > div.list > form > table > tbody > tr")
+tr_tags = soup.find_all("tr")
 notices = []
+for tr_tag in tr_tags[1:10]:
+    td_tags = tr_tag.find_all("td")
+    
+    if td_tags[0].find("img"):
+        pass
+        #continue
+    title_token = td_tags[1].get_text().split()
+    if "글로벌" in title_token[0]:
+        continue
+    else:
+        title = " ".join(title_token[1:])
+    date = "/".join(td_tags[3].get_text().strip().split("-")[1:])
+    link = "http://builder.hufs.ac.kr/user/" + td_tags[1].find("a").get("href")
+    notices.append([date, title, link])
+    if len(notices) >= 5:
+        break
+   
+"""
 for tr_tag in tr_tags:
     td_tags = tr_tag.select("td")
-    if td_tags[0].select("img"):
+    if td_tags.select("img"):
         continue
     title_token = td_tags[1].get_text().split()
     if "글로벌" in title_token[0]:
@@ -24,7 +42,6 @@ for tr_tag in tr_tags:
     notices.append([date, title, link])
     if len(notices) >= 5:
         break
-
+"""
 for notice in notices:
     print(notice)
-   
