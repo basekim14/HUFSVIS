1b43b6f450cc5ddaabc4ba8366b3abdbcc48abc9
KimGicHo <basekim14@gmail.com>
2020-07-07 17:18:33 +0900

    Update main.py 0707

--- crawling.py
+++ crawling.py
@@ -2,28 +2,27 @@
 from bs4 import BeautifulSoup
 import requests
 
-url = "http://builder.hufs.ac.kr/user/indexSub.action?codyMenuSeq=37080&siteId=hufs&menuType=T&uId=4&sortChar=AB&linkUrl=04_0202"
-res = requests.get(url)
+URL = "http://builder.hufs.ac.kr/user/indexSub.action?codyMenuSeq=37080&siteId=hufs&menuType=T&uId=4&sortChar=AB&linkUrl=04_0202"
+res = requests.get(URL)
 soup = BeautifulSoup(res.content, "html.parser")
 
 # tr_tags = soup.select("#board-container > div.list > form > table > tbody > tr")
 tr_tags = soup.find_all("tr")
-notices = []
-for tr_tag in tr_tags[1:10]:
+NOTICES = []
+for tr_tag in tr_tags[1:]:
     td_tags = tr_tag.find_all("td")
     
     if td_tags[0].find("img"):
-        pass
-        #continue
+        continue
     title_token = td_tags[1].get_text().split()
     if "글로벌" in title_token[0]:
         continue
     else:
         title = " ".join(title_token[1:])
     date = "/".join(td_tags[3].get_text().strip().split("-")[1:])
-    link = "http://builder.hufs.ac.kr/user/" + td_tags[1].find("a").get("href")
-    notices.append([date, title, link])
-    if len(notices) >= 5:
+    url = "http://builder.hufs.ac.kr/user/" + td_tags[1].find("a").get("href")
+    NOTICES.append({"date": date, "title": title, "url": url})
+    if len(NOTICES) >= 5:
         break
    
 """
@@ -43,5 +42,4 @@ for tr_tag in tr_tags:
     if len(notices) >= 5:
         break
 """
-for notice in notices:
-    print(notice)
+print(NOTICES)
--- main.py
+++ main.py
@@ -3,36 +3,54 @@
 #                basekim14  Project               #
 #                   - HUFSVIS -                   #
 #      HUFS's Academic Notice Crawling Widget     #
-# # # # # # # # # # # # # # # # # # # # # # # # # #
+# # # # # # # # # # # # # # # # # # # # # # # # # #ㅗ
 
 import appex, ui
+import os
+import webbrowser
+from math import ceil, floor
 import requests
 from bs4 import BeautifulSoup
 
-def clear_button_tapped(sender):
-	sender.superview["text_label"].text = "Clipboard:\n"
+COLS = 1
+ROWS = 3
+
+NOTICES = [{'date': '07/03', 'title': '2020-1학기 이중전공 변경 배정 확정 공고', 'url': 'http://builder.hufs.ac.kr/user/boardList.action?command=view&page=1&boardId=109336176&boardSeq=139236136'}, {'date': '06/29', 'title': '국내대학 학점교류 안내', 'url': 'http://builder.hufs.ac.kr/user/boardList.action?command=view&page=1&boardId=109336176&boardSeq=139024031'}, {'date': '06/29', 'title': '2020-여름계절학기 재수강 연결 일정 공고', 'url': 'http://builder.hufs.ac.kr/user/boardList.action?command=view&page=1&boardId=109336176&boardSeq=139000702'}, {'date': '06/29', 'title': '2020-1학기 원격수업 운영 및 우수수업사례 설문조사', 'url': 'http://builder.hufs.ac.kr/user/boardList.action?command=view&page=1&boardId=109336176&boardSeq=138997975'}, {'date': '06/19', 'title': '2020-여름계절학기 등록기간 연장 및 환불 안내', 'url': 'http://builder.hufs.ac.kr/user/boardList.action?command=view&page=1&boardId=109336176&boardSeq=138611252'}]
+
+class LauncherView (ui.View):
+	def __init__(self, notices, *args, **kwargs):
+		row_height = 110 / ROWS
+		super().__init__(self, frame=(0, 0, 300, ceil(len(notices) / COLS) * row_height), *args, **kwargs)
+		self.buttons = []
+		for n in notices:
+			btn = ui.Button(title=(" " + n["date"] + " " + n["title"]).ljust(100), font=("Menlo", 12), name=n['url'], alignment = ui.ALIGN_LEFT, action=self.button_action, bg_color=n.get("color", "#000000"), tint_color="#ffffff", corner_radius=6)
+			self.add_subview(btn)
+			self.buttons.append(btn)
+		"""
+		reload date btn	
+		"""
+
+	def layout(self):
+		bw = self.width / COLS
+		bh = floor(self.height / ROWS) if self.height <= 130 else floor(110 / ROWS)
+		for i, btn in enumerate(self.buttons):
+			btn.frame = ui.Rect(i%COLS * bw, i//COLS * bh, bw, bh).inset(2, 2)
+			btn.alpha = 1 if btn.frame.max_y < self.height else 0
+	
+	def button_action(self, sender):
+		webbrowser.open(sender.name)
+		
+	def crawling_action(self):
+	    return
 
 def main():
-	v = ui.View(frame=(0, 0, 320, 220))
-	label = ui.Label(frame=(8, 0, 320 - 44 - 8, 220), flex="wh")
-	label.name = "text_label"
-	label.font = ("Menlo", 12)
-	label.number_of_lines = 0
-	v.add_subview(label)
-	clear_btn = ui.Button(frame=(320-44, 0, 44, 220), flex="hl")
-	clear_btn.image = ui.Image.named('iow:ios7_trash_32')
-	clear_btn.action = clear_button_tapped
-	v.add_subview(clear_btn)
-	appex.set_widget_view(v)
-	text = "이쁜 한글 폰트"
-	label.text = "Clipboard:\n" + text
+	widget_name = __file__ + str(os.stat(__file__).st_mtime)
+	v = appex.get_widget_view()
+	# Optimization: Don't create a new view if the widget already shows the launcher.
+	if v is None or v.name != widget_name:
+		v = LauncherView(NOTICES)
+		v.name = widget_name
+		appex.set_widget_view(v)
 
-notices = [['07/03', '2020-1학기 이중전공 변경 배정 확정 공고', 'http://builder.hufs.ac.kr/user/boardList.action?command=view&page=1&boardId=109336176&boardSeq=139236136'],
-           ['06/29', '국내대학 학점교류 안내', 'http://builder.hufs.ac.kr/user/boardList.action?command=view&page=1&boardId=109336176&boardSeq=139024031'],
-           ['06/29', '2020-여름계절학기 재수강 연결 일정 공고', 'http://builder.hufs.ac.kr/user/boardList.action?command=view&page=1&boardId=109336176&boardSeq=139000702'],
-           ['06/29', '2020-1학기 원격수업 운영 및 우수수업사례 설문조사', 'http://builder.hufs.ac.kr/user/boardList.action?command=view&page=1&boardId=109336176&boardSeq=138997975'],
-           ['06/19', '2020-여름계절학기 등록기간 연장 및 환불 안내', 'http://builder.hufs.ac.kr/user/boardList.action?command=view&page=1&boardId=109336176&boardSeq=138611252']]
-
-if __name__ == "__main__":
+if __name__ == '__main__':
 	main()
-	
